0fc28694af193c65bf6bbba72eca705cad82f2cb
Teja srinivas <102207778+20X41A0586@users.noreply.github.com>
2025-10-12 17:23:11 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,27 @@
+# Update system
+sudo yum update -y
 
-#! /bin/bash
-sudo apt update -y
-sudo apt install -y openjdk-17-jdk
-java -version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# Install Git + Maven
+sudo yum install git maven -y
 
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y
-sudo apt install -y jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+# Install Java 11
+sudo amazon-linux-extras install java-openjdk11 -y
+
+# Add Jenkins repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# Install Jenkins
+sudo yum install jenkins -y
+
+# Start Jenkins service
+sudo service jenkins start
+
+# Enable auto-start on boot
+sudo chkconfig jenkins on
+
+# Check Jenkins status
+sudo service jenkins status
+
+
